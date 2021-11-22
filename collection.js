3619fb9b520c621ef789466020f3653304830b83
benjp009 <patin.benjamin@gmail.com>
2021-11-22 16:48:19 +0100

    Fix linters 1

--- collection.js
+++ collection.js
@@ -1,38 +1,38 @@
+// eslint-disable-next-line no-unused-vars
 const bookTitle = document.querySelector('.book-title');
 const bookAuthor = document.querySelector('.book-author');
-const addBook = document.querySelector('#add-book')
+const addBook = document.querySelector('#add-book');
 
-let protoObjet = {
-  title: "",
-  author: "",
-}
+const protoObjet = {
+  title: '',
+  author: '',
+};
 
 const removeBook = () => {
-  let listBooks = JSON.parse(localStorage.getItem('BookList') || "[]" );
-  if(listBooks.length > 0) {
+  let listBooks = JSON.parse(localStorage.getItem('BookList') || '[]');
+  if (listBooks.length > 0) {
     const removeButtons = document.querySelectorAll('#remove-book');
-    removeButtons.forEach(removeButton => {
+    removeButtons.forEach((removeButton) => {
       removeButton.addEventListener('click', () => {
-        listBooks = listBooks.filter((book, index) => index !== parseInt(removeButton.getAttribute('data-atr')));
+        listBooks = listBooks.filter((book, index) => index !== parseInt(removeButton.getAttribute('data-atr'), 10));
         localStorage.setItem('BookList', JSON.stringify(listBooks));
         displayBooks();
-      })
+      });
     });
-
   }
-}
+};
 
 const displayBooks = () => {
-  let listBooks = JSON.parse(localStorage.getItem('BookList') || "[]" );
-  if (listBooks.length == 0) {
+  const listBooks = JSON.parse(localStorage.getItem('BookList') || "[]" );
+  if (listBooks.length === 0) {
     document.getElementById('book-list').innerHTML = `
       <div class="book">
         <p class="no-book">No books</p>
         <hr>
       </div>
-    `
+    `;
   } else {
-    let bookString = "";
+    let bookString = '';
     for (let i = 0; i < listBooks.length; i += 1) {
       bookString += `
         <div class="book">
@@ -41,29 +41,29 @@ const displayBooks = () => {
           <button id="remove-book" type="button" data-atr="${i}">Remove</button>
           <hr>
         </div>
-      `
+      `;
     }
     document.getElementById('book-list').innerHTML = bookString;
     removeBook();
   }
-}
+};
 
 displayBooks();
 
 const addBooky = () => {
   addBook.addEventListener('click', () => {
-    let newBook = Object.create(protoObjet, {})
+    const newBook = Object.create(protoObjet, {});
     const bookTitle = document.querySelector('.book-title');
     const bookAuthor = document.querySelector('.book-author');
     newBook.title = bookTitle.value;
     newBook.author = bookAuthor.value;
-    let listBooks = JSON.parse(localStorage.getItem('BookList') || "[]" );
+    const listBooks = JSON.parse(localStorage.getItem('BookList') || '[]');
     listBooks.push(newBook);
     localStorage.setItem('BookList', JSON.stringify(listBooks));
     displayBooks();
   });
-}
+};
 
 window.onload = () => {
   addBooky();
-}
+};

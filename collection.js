182f73ffb686bc0b1e0b92fcfd4decbe3697aa9f
benjp009 <patin.benjamin@gmail.com>
2021-11-22 16:35:51 +0100

    Clear js file

--- collection.js
+++ collection.js
@@ -11,22 +11,20 @@ const removeBook = () => {
   let listBooks = JSON.parse(localStorage.getItem('BookList') || "[]" );
   if(listBooks.length > 0) {
     const removeButtons = document.querySelectorAll('#remove-book');
-    console.log(removeButtons);
     removeButtons.forEach(removeButton => {
       removeButton.addEventListener('click', () => {
-        listBooks = listBooks.filter((book, index) => index !== parseInt(removeButton.getAttribute('data-atr')));      
+        listBooks = listBooks.filter((book, index) => index !== parseInt(removeButton.getAttribute('data-atr')));
         localStorage.setItem('BookList', JSON.stringify(listBooks));
         displayBooks();
       })
     });
-  
+
   }
 }
 
 const displayBooks = () => {
   let listBooks = JSON.parse(localStorage.getItem('BookList') || "[]" );
   if (listBooks.length == 0) {
-    console.log("I work");
     document.getElementById('book-list').innerHTML = `
       <div class="book">
         <p class="no-book">No books</p>
@@ -45,13 +43,11 @@ const displayBooks = () => {
         </div>
       `
     }
-    
     document.getElementById('book-list').innerHTML = bookString;
     removeBook();
   }
 }
 
-// if (listBooks[{0}].length === 0) {
 displayBooks();
 
 const addBooky = () => {
@@ -61,126 +57,13 @@ const addBooky = () => {
     const bookAuthor = document.querySelector('.book-author');
     newBook.title = bookTitle.value;
     newBook.author = bookAuthor.value;
-
-    // console.log(newBook)
     let listBooks = JSON.parse(localStorage.getItem('BookList') || "[]" );
     listBooks.push(newBook);
     localStorage.setItem('BookList', JSON.stringify(listBooks));
     displayBooks();
-    console.log(listBooks);
   });
 }
-
-
 
 window.onload = () => {
   addBooky();
 }
-
-// const addBook = {
-//   title: "some book Title",
-//   author: "Some Author"
-// };
-
-// const newCollection = Object.assign(listBooks, addBook);
-
-// console.log(listBooks);
-// console.log(addBook)
-
-// const employee = Object.create(listBooks,
-//   {
-//     title: { value: bookTitle },
-//     author: { value: bookAuthor }
-//   });
-
-// console.log(employee);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const storageAvailable = (type) => {
-//   let storage;
-//   try {
-//     storage = window[type];
-//     const x = '__storage_test__';
-//     storage.setItem(x, x);
-//     storage.removeItem(x);
-//     return true;
-//   } catch (e) {
-//     return (
-//       e instanceof DOMException
-//       && (e.code === 22
-//         || e.code === 1014
-//         || e.name === 'QuotaExceededError'
-//         || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
-//       && storage
-//       && storage.length !== 0
-//     );
-//   }
-// };
-
-// if (storageAvailable('localStorage') && localStorage.getItem('bookLocalStorage')) {
-//   const bookLocalStorage = JSON.parse(localStorage.getItem('bookLocalStorage'));
-//   bookTitle.value = bookLocalStorage.title;
-//   bookAuthor.value = bookLocalStorage.author;
-// }
-
-// const populateStorage = () => {
-//   if (storageAvailable('localStorage')) {
-//     const bookLocalStorage = {
-//       title: bookTitle.value,
-//       author: bookAuthor.value,
-//     };
-//     const bookLocalStorageStr = JSON.stringify(bookLocalStorage);
-//     localStorage.setItem('bookLocalStorage', bookLocalStorageStr);
-//   }
-// };
-// title.onchange = populateStorage;
-// author.onchange = populateStorage;

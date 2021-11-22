eaafc8674638eefba199545976e2fed56d1a6c36
benjp009 <patin.benjamin@gmail.com>
2021-11-22 10:38:54 +0100

    Create html and collection.js

---
+++ collection.js
@@ -0,0 +1,13 @@
+const listBooks = [
+  {
+    bookOneTitle: "I am John",
+    bookOneAuthor: "John Lennon"
+  }
+]
+
+const listBooks = [
+  bookOne = {
+    title: "I am John",
+    author: "John Lennon"
+  },
+]

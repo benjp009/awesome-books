15b00695cd8af7cc78e16e75109af308472a5529
charlesgobina <charles.gobina@gmail.com>
2021-11-23 10:53:49 +0100

    Add book class and methods

--- collection.js
+++ collection.js
@@ -1,8 +1,63 @@
-// eslint-disable-next-line no-unused-vars
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  };
+
+  addBook() {
+    let book = {
+      title: this.title,
+      author: this.author
+    }
+
+    let listBooks = JSON.parse(localStorage.getItem('BookList') || '[]');
+    listBooks.push(book);
+    localStorage.setItem('BookList', JSON.stringify(listBooks));
+  }
+
+  removeBook(id) {
+    let listBooks = JSON.parse(localStorage.getItem('BookList') || '[]');
+    listBooks = listBooks.filter((book, index) => index !== parseInt(id, 10));
+    localStorage.setItem('BookList', JSON.stringify(listBooks));
+  }
+
+  displayBooks() {
+    const listBooks = JSON.parse(localStorage.getItem('BookList') || '[]');
+    if (listBooks.length === 0) {
+      return `
+        <div class="book">
+          <p class="no-book">No books</p>
+          <hr>
+        </div>
+      `;
+    } else {
+      let bookString = '';
+      for (let i = 0; i < listBooks.length; i += 1) {
+        bookString += `
+          <div class="book">
+            <p>${listBooks[i].title}</p>
+            <p>${listBooks[i].author}</p>
+            <button id="remove-book" type="button" data-atr="${i}">Remove</button>
+            <hr>
+          </div>
+        `;
+      }
+      return bookString
+    }
+  }
+}
+
 const bookTitle = document.querySelector('.book-title');
-// eslint-disable-next-line no-unused-vars
 const bookAuthor = document.querySelector('.book-author');
 const addBook = document.querySelector('#add-book');
+
+
+addBook.addEventListener('click', () => {
+  let book = new Book(bookTitle.value, bookAuthor.value);
+  book.addBook();
+  document.getElementById('book-list').innerHTML = book.displayBooks();
+})
+
 
 const protoObjet = {
   title: '',
